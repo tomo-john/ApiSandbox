6ba0165812ab0a4af86f9db8d569aae8e4e86b26
tomo-john <keririnkery.kery@gmail.com>
2026-09-22 17:33:51 +0900

    chore: User, Dogモデルのリレーション定義(+fillable)

--- app/Models/Dog.php
+++ app/Models/Dog.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Dog extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'breed',
+        'birthdate',
+        'weight',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
